bdf4ab042a333e33a7104586fb9b1446ad18ce02
Daniel Flores Diaz <daniel.flores@Daniel-Mac-Work.local>
2023-11-13 21:35:47 -0600

    feat: Create a css file for center the box and create a list of elements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import './myStyle.css'
+import {
+    ChakraProvider,
+    Box,
+    List,
+    ListItem,
+    ListIcon,
+    OrderedList,
+    UnorderedList,
+} from '@chakra-ui/react';
 const App = () => {
- return <h1>This is my React app! Hola como estas?</h1>;
+    return (
+        <ChakraProvider>
+            <Box className="BoxList" borderWidth='1px' rounded='lg' p='50' boxShadow='base'>
+                <div >To-Do List:
+                    <UnorderedList id="list">
+                        <ListItem>Lorem ipsum dolor sit</ListItem>
+                    </UnorderedList>
+                </div>
+            </Box>
+        </ChakraProvider>
+      );
  }
 ReactDOM.render(<App />, document.getElementById('app'));
